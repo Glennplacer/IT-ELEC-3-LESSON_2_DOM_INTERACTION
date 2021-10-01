ca414c702260545bacf6e81b6a7998105dd87390
Glenn Anrei Placer <glennplacer1@gmail.com>
2021-10-01 18:40:50 +0800

    Topic9-adding methods and age

--- Topic9-Practice_Data_Binding_Problem/app.js
+++ Topic9-Practice_Data_Binding_Problem/app.js
@@ -2,6 +2,12 @@ const app = Vue.createApp({
     data() {
         return {
             myName: 'Glenn Anrei Placer',
+            myAge: 20
+        };
+    },
+    methods: {
+        Edad() {
+            return this.myAge + 5;
         }
     }
 });
