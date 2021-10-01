c9b9d94a2a9356f7e7018a38e2159b5211f22812
Glenn Anrei Placer <glennplacer1@gmail.com>
2021-10-01 18:48:43 +0800

    Topic 9-add image and input value

--- Topic9-Practice_Data_Binding_Problem/app.js
+++ Topic9-Practice_Data_Binding_Problem/app.js
@@ -2,12 +2,21 @@ const app = Vue.createApp({
     data() {
         return {
             myName: 'Glenn Anrei Placer',
-            myAge: 20
+            myAge: 20,
+            url: 'https://vuejs.org/images/logo,png'
         };
     },
     methods: {
         Edad() {
             return this.myAge + 5;
+        },
+        Uno() {
+            const random = Math.random();
+            if(random < 0.5) {
+                return '0'
+            } else {
+                return '1'
+            }
         }
     }
 });
